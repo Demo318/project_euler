1c1c1472443ebd405e96129eafd0edf5e9276a45
Devin Mork <devin.mork@gmail.com>
2018-01-01 21:47:02 -0600

    Initial commit (again).

---
+++ 001/001.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+# Written in Python 3.6.3
+# Execute in bash using "./001.py"
+
+
+# Project Euler
+# Problem 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
